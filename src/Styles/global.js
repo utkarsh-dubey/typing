1271f2150940fb0c2c1b5584073dc7a828436104
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2022-11-08 21:38:23 +0530

    day1

---
+++ src/Styles/global.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+body{
+    background: red;
+
+}
+.heading{
+    font-size: 10rem;
+}
+
+
+`;
